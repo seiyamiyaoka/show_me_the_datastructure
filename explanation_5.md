4a3beec30882855cba8977527dea4fb037f51a7b
宮岡聖也 <miyaokakiyoshinari@miyaokakiyoshinarinoMacBook-Pro-2.local>
2020-10-02 09:16:31 +0900

    modify block structor

--- explanation_5.md
+++ explanation_5.md
@@ -5,7 +5,11 @@
   - I chose LinkedList because I wanted to use data connections.
 
 ## About Order complexity
-Computational complexity of O (n) is required to acquire and add blocks.
+$O(1)$
+Set "head" and "tail" in the BlockChain constructor.
+The last block is stored by the block_chain object.
+
+The Order complexity is $O(1)$ because tail can be accessed when adding a block.
 
 ## About implementation
 When adding a new block, it loops to the end and sets the hash value of the last block to the latest block.
--- problem5.py
+++ problem5.py
@@ -6,16 +6,17 @@ random.seed(1)
 class BlockChain:
   def __init__(self, head):
     self.head = head
-  def add_chain(self, new_block):
-    block = self.head
+    self.tail = head
 
-    while block.next is not None:
-      block = block.next
+  def add_chain(self, new_block):
+    block = self.tail
+
     new_block.previous_hash = block.hash
     block.next = new_block
+    self.tail = new_block
 
 class Block:
-  def __init__(self, timestamp, data, previous_hash):
+  def __init__(self, timestamp, data, previous_hash=None):
     self.timestamp = timestamp
     self.data = data
     self.previous_hash = previous_hash
@@ -29,15 +30,36 @@ class Block:
     return sha.hexdigest()
 
 chain = BlockChain(Block(12, 'hello', 0))
-chain.add_chain(Block(12, 'kojiro', chain.head.hash))
-chain.add_chain(Block(None, 'kojiro', chain.head.hash))
-
+chain.add_chain(Block("", 'kojiro'))
+chain.add_chain(Block(None, 'kojiro'))
+chain.add_chain(Block(None, None))
+chain.add_chain(Block("", ""))
 
 block = chain.head
 while block is not None:
+  print("check: hash")
   print(block.hash)
-  # first: 8f10e410fdef7119b03a3c302117e3732b5e8b40d7759b825ec19400be3f9a2f
-  # second: 8ad0de39ca29445c885a6224add8f03ba0a0d28dba5c7f2f963b96fce4553947
-  # third: ddd22fb235dffc9cb3716c02c0e56d04acd27e0be5feef87ab0a406f42371f7b
+  print("check: previous_hash")
+  print(block.previous_hash)
+  # check: hash
+  # 8f10e410fdef7119b03a3c302117e3732b5e8b40d7759b825ec19400be3f9a2f
+  # check: previous_hash
+  # 0
+  # check: hash
+  # 8ad0de39ca29445c885a6224add8f03ba0a0d28dba5c7f2f963b96fce4553947
+  # check: previous_hash
+  # 8f10e410fdef7119b03a3c302117e3732b5e8b40d7759b825ec19400be3f9a2f
+  # check: hash
+  # ddd22fb235dffc9cb3716c02c0e56d04acd27e0be5feef87ab0a406f42371f7b
+  # check: previous_hash
+  # 8ad0de39ca29445c885a6224add8f03ba0a0d28dba5c7f2f963b96fce4553947
+  # check: hash
+  # 99c5875334f709b0ecae8ca1ffdc7b2cc03d43665ac7ae2a21ade613a3ff66c4
+  # check: previous_hash
+  # ddd22fb235dffc9cb3716c02c0e56d04acd27e0be5feef87ab0a406f42371f7b
+  # check: hash
+  # bf25b109b4381e9041ac2ece0fa2b522a88c06ef8ea0434770cacf8ec98a220a
+  # check: previous_hash
+  # 99c5875334f709b0ecae8ca1ffdc7b2cc03d43665ac7ae2a21ade613a3ff66c4
   block = block.next
 
