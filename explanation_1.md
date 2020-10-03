c99f44540828ae143b6884e621b9c03a32e1f1cb
宮岡聖也 <miyaokakiyoshinari@miyaokakiyoshinarinoMacBook-Pro-2.local>
2020-10-03 11:20:07 +0900

    fix: space complexity and add edgecase

--- explanation_1.md
+++ explanation_1.md
@@ -12,23 +12,14 @@ The "get" and "set" methods access the cache.
 The Order complexity is $O(1)$ because the data can be specified at once by the key.
 
 ## About Space complexity
-`self.capacity` is 28bit because it expects int.
-
-```python
->>> sys.getsizeof(4)
-28 # bytes
-```
-
-self.cache is a dictionary.
-The maximum storage capacity is 5 according to the rules.
-
-```python
-sys.getsizeof(d) # d have 5 keys dictionary
-376 # bytes
-```
-
-Space complexity is about 400bytes.
-
+- __init__(self, capacity)
+  - $O(1)$. This is because one value is set for self.capacity and self.cache. => $O(2)$ => $O(1)$
+- get(self, key):
+  - $O(n)$. The key received as an argument requires a capacity of 1. The self.cache will increase by the size of capacity at the maximum. therefor capacity is n. => $O(n + 1)$ => $O(n)$
+- set(self, key, value)
+  - $O(n)$. It's almost the same as get method.
+- _is_valid_capacity(self, capacity)
+  - $O(1)$
 ## About implementation
 
 The big feature is to consider the order while using hashmap.
--- problem1.py
+++ problem1.py
@@ -1,6 +1,6 @@
 class LRU_Cache(object):
   def __init__(self, capacity):
-    self.capacity = capacity
+    self.capacity = self._is_valid_capacity(capacity)
     self.cache = {}
 
   def get(self, key):
@@ -19,11 +19,20 @@ class LRU_Cache(object):
     if key in self.cache:
       del self.cache[key]
     else:
-      if self.capacity <= 0:
-        del self.cache[list(self.cache.keys())[0]]
-    self.capacity -= 1
-    self.cache[key] = value
+      if self.capacity is not None:
+        if self.capacity <= 0:
+          del self.cache[list(self.cache.keys())[0]]
+        self.capacity -= 1
+        self.cache[key] = value
 
+  def _is_valid_capacity(self, capacity):
+    if capacity is None or capacity <= 0:
+      print("Please add a positive integer")
+      return None
+    else:
+      return capacity
+
+# case 1 max capacity 5
 our_cache = LRU_Cache(5)
 
 our_cache.set(1, 1)
@@ -50,3 +59,39 @@ our_cache.set(5, 5)
 our_cache.set(6, 6)
 print(our_cache.get(3))
 # returns -1
+
+## case 2 max capacity negative number
+our_cache = LRU_Cache(-1)
+# print Please add a positive integer
+our_cache.set(1, 1)
+our_cache.set(None, 1)
+# print "You can't set None!!"
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+our_cache.set(5, 5)
+
+## case 3 capacity is None
+our_cache = LRU_Cache(None)
+# print Please add a positive integer
+our_cache.set(1, 1)
+our_cache.set(None, 1)
+# print "You can't set None!!"
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+our_cache.set(5, 5)
+
+## case 4 large capacity(1000)
+
+our_cache = LRU_Cache(1000)
+
+for i in range(1000):
+  our_cache.set(i, i)
+
+print(our_cache.get(0))
+# 0
+our_cache.set(1000, 1000)
+import pdb; pdb.set_trace()
+print(our_cache.get(1))
+# -1
